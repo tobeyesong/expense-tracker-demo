f65ba9ef0eaf0bad01037c451b11dd91431abe26
Toan Lam <toanlam01@gmail.com>
2021-09-27 12:07:36 -0700

    update styling on cards, and fix balance

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -36,13 +36,15 @@ const Cards = () => {
   // const { total, chartData } = useTransactions(title);
 
   //BALANCE
-  const balance = transactions.reduce(
-    (acc, currVal) =>
-      currVal.category === "expense"
-        ? acc - currVal.amount
-        : acc + currVal.amount,
-    0
-  );
+  const balance = transactions
+    .reduce(
+      (acc, currVal) =>
+        currVal.category === "expense"
+          ? acc - currVal.amount
+          : acc + currVal.amount,
+      0
+    )
+    .toFixed(2);
   //INCOME
   const income = incomeTransactions
     .reduce((acc, currVal) => (acc += +currVal.amount), 0)
@@ -84,45 +86,47 @@ const Cards = () => {
           Overview
         </h2>
         <>
-          <div className='grid grid-cols-1 gap-5 mt-2 sm:grid-cols-2 lg:grid-cols-3'>
-            {/* Card */}
-            {cards.map((card) => (
-              <div
-                key={card.name}
-                className='overflow-hidden transition duration-200 ease-in bg-white rounded-lg shadow hover:shadow-lg'>
-                <div className={classNames(cardStyles[card.style], "p-3")}>
-                  <div className='flex items-center '>
-                    <div className='flex-shrink-0'>
-                      <card.icon
-                        className='w-6 h-6 text-gray-400'
-                        aria-hidden='true'
-                      />
+          <div className='p-4 bg-gray-700 rounded-lg'>
+            <div className='grid grid-cols-1 gap-5 mt-2 text-gray-50 sm:grid-cols-2 lg:grid-cols-3'>
+              {/* Card */}
+              {cards.map((card) => (
+                <div
+                  key={card.name}
+                  className='overflow-hidden transition duration-200 ease-in transform rounded-lg hover:bg-gray-600 hover:-translate-y-2 hover:shadow-lg'>
+                  <div className={classNames(cardStyles[card.style], "p-3")}>
+                    <div className='flex items-center '>
+                      <div className='flex-shrink-0'>
+                        <card.icon
+                          className='w-6 h-6 text-gray-400'
+                          aria-hidden='true'
+                        />
+                      </div>
+                      <div className='flex-1 w-0 ml-5'>
+                        <dl>
+                          <dt className='text-sm font-medium truncate '>
+                            {card.name}
+                          </dt>
+                          <dd>
+                            <div className='text-lg font-medium text-gray-50'>
+                              $ {numberWithCommas(card.amount)}
+                            </div>
+                          </dd>
+                        </dl>
+                      </div>
                     </div>
-                    <div className='flex-1 w-0 ml-5'>
-                      <dl>
-                        <dt className='text-sm font-medium truncate '>
-                          {card.name}
-                        </dt>
-                        <dd>
-                          <div className='text-lg font-medium text-gray-900'>
-                            $ {numberWithCommas(card.amount)}
-                          </div>
-                        </dd>
-                      </dl>
+                  </div>
+                  <div className='px-5 py-3 bg-gray-600 shadow-md'>
+                    <div className='text-sm'>
+                      <a
+                        href={card.href}
+                        className='font-mediumover:text-cyan-900'>
+                        View all
+                      </a>
                     </div>
                   </div>
                 </div>
-                <div className='px-5 py-3 bg-gray-50'>
-                  <div className='text-sm'>
-                    <a
-                      href={card.href}
-                      className='font-mediumover:text-cyan-900'>
-                      View all
-                    </a>
-                  </div>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </>
       </div>
